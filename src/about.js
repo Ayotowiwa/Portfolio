2b99e25f37251ccaddb66a3995d6188614697260
Nuel <christlightsied@gmail.com>
2023-11-28 12:12:44 +0000

    CV innit

--- src/about.js
+++ src/about.js
@@ -1,18 +1,22 @@
 import React from "react";
 import imageMe from "./imagesvg/mrme.png";
+// import CV_PDF from "./imagesvg/";
 
 function AboutMe() {
   return (
     <div className="about" id="about-section">
       <img src={imageMe} className="messi" />
       <div className="about-text">
-        <h4>Who am i ?</h4>
+        <h4>Who am I?</h4>
         <h2>About me</h2>
         <p>
           I'm a web designer / developer based in Lagos, Nigeria. I have a
           passion for web design and love to create for web and mobile devices.
         </p>
-        <button className="lightbutton">View my CV</button>
+
+        <a href={CV_PDF} target="_blank" rel="noopener noreferrer" download>
+          <button className="lightbutton">View my CV</button>
+        </a>
       </div>
     </div>
   );
